0bae1dc277f9e5fcd14bd6440b71500be97e87a2
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-05-31 19:58:53 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
-from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+"""
+This module contains endpoint(route) stats
+"""
 from models import storage
-
-
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
-
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
+from flask import Flask
+from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
+def status():
+    """
+    Returns a JSON stats
+    """
+    return jsonify({"stats": "OK"})
 
-if __name__ == "__main__":
-    pass
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
